a58f90e9809fc088b4ab52e9a3e282189a2cb69f
alumno <florespedromartin@gmail.com>
2021-05-05 21:03:40 -0300

    Arreglo del menu de windows

--- src/Windows/menu.py
+++ src/Windows/menu.py
@@ -2,10 +2,10 @@ import PySimpleGUI as sg
 #Creador de ventanas
 def build():
     layout = [
-        [sg.Button("play",size=(50,2), key="-play-")]
-        [sg.Button("settings",size=(50,2), key="-settings-")]
-        [sg.Button("score",size=(50,2), key="-score-")]
-        [sg.Button("exit",size=(50,2), key="-exit-")]
+        [sg.Button("play",size=(50,2), key="-play-")],
+        [sg.Button("settings",size=(50,2), key="-settings-")],
+        [sg.Button("score",size=(50,2), key="-score-")],
+        [sg.Button('Exit',size=(50,2), key="-exit-")]
     ]
 
     board = sg.Window("Juego de la memoria").Layout(layout)
