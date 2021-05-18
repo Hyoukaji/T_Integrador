4b947db571d64824ebbe2ae9e5c73bc5f42df937
alumno <florespedromartin@gmail.com>
2021-05-18 04:26:21 -0300

    sera que ahora si?

--- src/Components/cant_casillas3.py
+++ src/Components/cant_casillas3.py
@@ -1,34 +1,30 @@
 import PySimpleGUI as sg
 from src.Windows import introduzca_texto
-from src.Handlers import set_cant_casillas
-from src.Components import cant_casillas2
+from src.Components import cant_casillas3
 
 def start():
 
-    window = loop()
+    window, x = loop()
     window.close()
-
+    return x
 def loop():
     t = "Cantidad de casillas"
-    tx = "Introduzca 1 número para elevarlo al cuadrado y crear el tablero del nivel 1"
+    tx = "Introduzca 1 número para elevarlo al cuadrado y crear el tablero del nivel 3"
     b = "Actualizar"
     window = introduzca_texto.build(tx, b, t)
 
     while True:
         event, values = window.read()
 
-        if event in (sg.WINDOW_CLOSED, "-exit-"):
-            break
+
 
         if event == "-update-":
             text_input = values[0]
             try:
                 x = int(text_input)
-                sg.popup("Seteaste la cantidad de casillas del nivel 1 en:", x*x)
-                y, z= cant_casillas2.start()
-                set_cant_casillas.start(x, y, z)
+                sg.popup("Seteaste la cantidad de casillas del nivel 3 en:", x*x)
                 break
             except:
                 sg.popup("Tienes que ingresar un número entero")
 
-    return window
+    return window, x
