94f581a82289926ea1344ea5b628dd01cd4c0909
alumno <florespedromartin@gmail.com>
2021-05-18 14:15:57 -0300

    detalle en el set matchs

--- src/Handlers/set_matchs.py
+++ src/Handlers/set_matchs.py
@@ -8,7 +8,7 @@ ruta_archivo_2 = os.path.join("src/Archivos/", nom_arch_2)
 
 def start(valor):
     try:
-        unNick = get_nick_actual.start
+        unNick = get_nick_actual.start()
         dato_jugador_actual = {}
         with open(ruta_archivo_2, "r") as archivo_2:
             dato_jugador_actual = json.load(archivo_2)
